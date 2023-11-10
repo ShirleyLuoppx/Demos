3a758a86f2156e4d2f6f299e6539d591e036b0ee
luoxia <1184793244@qq.com>
2023-11-10 17:35:38 +0800

    update sth

--- app/src/main/java/com/ppx/demo/threejs/ThreeJsDemo.java
+++ app/src/main/java/com/ppx/demo/threejs/ThreeJsDemo.java
@@ -1,5 +1,6 @@
 package com.ppx.demo.threejs;
 
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -19,16 +20,29 @@ public class ThreeJsDemo extends BaseActivity {
 
     @Override
     public void initView() {
+        String url = "file:///android_asset/mytest/index.html";//简单的页面就能正常加载
+        //        url = "http://127.0.0.1:5500/mytest/index.html";
+
         m_Webgl3D_WV = findViewById(R.id.webview);
         m_WebSettings = m_Webgl3D_WV.getSettings();
         m_WebSettings.setJavaScriptEnabled(true);
+        m_WebSettings.setPluginState(WebSettings.PluginState.ON);
         m_WebSettings.setAllowUniversalAccessFromFileURLs(true);
         m_WebSettings.setAllowFileAccess(true);
+        m_WebSettings.setLoadsImagesAutomatically(true);
+        m_WebSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        m_WebSettings.setDefaultTextEncodingName("utf-8");
         //允许Dom缓存数据
-        m_WebSettings.setDomStorageEnabled(true);
-        String url = "file:///android_asset/mytest/index.html";//简单的页面就能正常加载
-//        url = "http://127.0.0.1:5500/mytest/index.html";
+        m_WebSettings.setDomStorageEnabled(true); // 开启 DOM storage API 功能
+        m_WebSettings.setDatabaseEnabled(true);   //开启 database storage API 功能
+        m_WebSettings.setAppCacheEnabled(true);//开启 Application Caches 功能
+        //设置对 viewport mate   的支持
+        m_WebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        m_WebSettings.setUseWideViewPort(true);
+        m_WebSettings.setLoadWithOverviewMode(true);
+
         m_Webgl3D_WV.setWebViewClient(new WebViewClient());
+        m_Webgl3D_WV.setWebChromeClient(new WebChromeClient());
         m_Webgl3D_WV.loadUrl(url);
     }
 
